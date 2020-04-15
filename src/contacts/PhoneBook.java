26f2c633e45a601a90c106f3424aa29560b5f43d
Mike <mikeavb@yandex.ru>
2020-04-16 00:29:17 +0300

    PhoneBook class implemented

--- src/contacts/PhoneBook.java
+++ src/contacts/PhoneBook.java
@@ -1,9 +1,15 @@
 package contacts;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public class PhoneBook {
+public class PhoneBook implements Serializable, Iterable<Contact> {
+    private static final long serialVersionUID = 8419192590332967500L;
+
     private List<Contact> list;
 
     public PhoneBook() {
@@ -18,8 +24,16 @@ public class PhoneBook {
         return list.remove(contact);
     }
 
+    public Contact remove(int i) {
+        return list.remove(i);
+    }
+
     public Contact get(int i) {
         return list.get(i);
+    }
+
+    public Contact set(int i, Contact contact) {
+        return list.set(i, contact);
     }
 
     public int size() {
@@ -28,5 +42,20 @@ public class PhoneBook {
 
     public boolean contains(Contact contact) {
         return list.contains(contact);
+    }
+
+    @Override
+    public Iterator<Contact> iterator() {
+        return list.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Contact> action) {
+        list.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Contact> spliterator() {
+        return list.spliterator();
     }
 }
